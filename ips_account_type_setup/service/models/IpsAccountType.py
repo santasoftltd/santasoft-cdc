63c8f876fb2ee8a015162f6a93a54e45a55736b7
Muhammad Umar <mumersajid1@gmail.com>
2025-01-22 22:34:11 +0500

    IPS Account type setup completed

---
+++ ips_account_type_setup/service/models/IpsAccountType.py
@@ -0,0 +1,53 @@
+from django.db import models
+
+
+class IpsAccountType(models.Model):
+    id = models.AutoField(primary_key=True, db_column='ID')  # Auto increment starting at 1000 (can be set via migration)
+    type = models.CharField(max_length=255, unique=True, db_column='Type')  # Unique string field
+
+    class Meta:
+        managed = False
+        db_table = '[Account Type]'  # Define the table name
+
+    def __str__(self):
+        return str(self.id) + ' - ' + self.type  # String representation of the model
+    
+    def create_object(self, data):
+        try:
+            self.type = data['type']
+            self.save()
+            return True, self
+        except Exception as error:
+            return False, error
+
+    def update_object(self, **kwargs):
+        try:
+            for field, value in kwargs.items():
+                if hasattr(self, field):
+                    setattr(self, field, value)
+            self.save()
+            return True, self
+        except Exception as error:
+            return False, error
+
+    def delete_object(self):
+        try:
+            self.delete()
+            return True, self
+        except Exception as error:
+            return False, error
+    
+    @classmethod
+    def get_all_objects(cls):
+        return cls.objects.all().values('id', 'type')
+    
+    @classmethod
+    def get_object_by_id(cls, id):
+        try:
+            return cls.objects.filter(id=id).values('id', 'type')[0]
+        except IndexError:
+            return None
+        
+    def save(self, *args, **kwargs):
+        super(IpsAccountType, self).save(*args, **kwargs)
+        return self.id
--- ips_account_type_setup/service/views.py
+++ ips_account_type_setup/service/views.py
@@ -1,43 +1,125 @@
 from rest_framework.views import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from .models.IpsAccountType import IpsAccountType
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class API(APIView):
 
-    # def view_branches(self, request):
-    #     try:
-    #         parameter = request.data['Parameter']
-    #         if all(key in parameter for key in ('Filter', 'Sort', "Offset", "Limit")):
-    #             if type(parameter['Offset']) is int and type(parameter['Limit']) is int and type(parameter['Filter']) is str and type(parameter['Sort']) is str:
-    #                 quary = Quary(filterQuery=self.getFilterQuery(parameter['Filter']), sortQuery=self.getSortQuery(parameter['Sort']))
-    #                 response_data = quary.getBranches()
-    #                 response = Response({
-    #                     'data': response_data
-    #                 }, status=status.HTTP_200_OK)
-    #             else:
-    #                 response = Response({
-    #                     'message': 'Invalid Parameter key type.'
-    #                 }, status=status.HTTP_400_BAD_REQUEST)
-    #         else:
-    #             response = Response({
-    #                 'message': 'Invalid request. Parameter Key is missing.'
-    #             }, status=status.HTTP_400_BAD_REQUEST)
-    #     except Exception as e:
-    #         response = Response({
-    #             'message': 'Branches service failed. Please try again or contact IT if the issue continues.'
-    #             }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    #     finally:
-    #         return response
+    def view_type(self, request):
+        try:
+            parameter = request.data['Parameter']
+            if all(key in parameter for key in ('Filter', 'Sort', "Offset", "Limit", "Id")):
+                if type(parameter['Offset']) is int and type(parameter['Limit']) is int and type(parameter['Id']) is int and type(parameter['Filter']) is str and type(parameter['Sort']) is str:
+                    # quary = Quary(filterQuery=self.getFilterQuery(parameter['Filter']), sortQuery=self.getSortQuery(parameter['Sort']))
+                    if parameter['Id'] == 0:
+                        response_data = IpsAccountType.get_all_objects()
+                    else:
+                        response_data = IpsAccountType.get_object_by_id(id=parameter['Id'])
+                    if response_data is None:
+                        response = Response({
+                        'message': 'Object not found.'
+                        }, status=status.HTTP_400_BAD_REQUEST)
+                    else:
+                        response = Response({
+                            'data': response_data
+                        }, status=status.HTTP_200_OK)
+                else:
+                    response = Response({
+                        'message': 'Invalid Parameter key type.'
+                    }, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                response = Response({
+                    'message': 'Invalid request. Parameter Key is missing.'
+                }, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as error:
+            response = Response({
+                'message': 'IPS Account type setup service failed. Error message: ' + error
+                }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        finally:
+            return response
+
+    def add_type(self, request):
+        try:
+            data = request.data['Data']
+            object = IpsAccountType()
+            is_success, error_message = object.create_object(data=data)
+            if is_success:
+                response = Response({
+                    'message': 'Success'
+                }, status=status.HTTP_200_OK)
+            else:
+                response = Response({
+                'message': error_message
+                }, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as error:
+            response = Response({
+                'message': 'IPS Account type setup service failed. Error message: ' + error
+                }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        finally:
+            return response
+
+    def update_type(self, request):
+        try:
+            data = request.data['Data']
+            try:
+                object = IpsAccountType.objects.get(id=data['id'])
+                is_success, error_message = object.update_object(id=data['id'],type=data['type'])
+                if is_success:
+                    response = Response({
+                        'message': 'Success'
+                    }, status=status.HTTP_200_OK)
+                else:
+                    response = Response({
+                    'message': error_message
+                    }, status=status.HTTP_400_BAD_REQUEST)
+            except ObjectDoesNotExist:
+                response = Response({
+                        'message': 'Object not found.'
+                    }, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as error:
+            response = Response({
+                'message': 'IPS Account type setup service failed. Error message: ' + str(error)
+                }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        finally:
+            return response
+
+    def remove_type(self, request):
+        try:
+            data = request.data['Data']
+            try:
+                object = IpsAccountType.objects.get(id=data['id'])
+                is_success, error_message = object.delete_object()
+                if is_success:
+                    response = Response({
+                        'message': 'Success'
+                    }, status=status.HTTP_200_OK)
+                else:
+                    response = Response({
+                    'message': error_message
+                    }, status=status.HTTP_400_BAD_REQUEST)
+            except ObjectDoesNotExist:
+                response = Response({
+                        'message': 'Object not found.'
+                    }, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as error:
+            response = Response({
+                'message': 'IPS Account type setup service failed. Error message: ' + error
+                }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        finally:
+            return response
 
     def post(self, request):
         if all(key in request.data for key in ('Timestamp', 'Function', "Parameter", "Data")):
-            if request.data['Function'] == 'AddPayment':
-                pass
-                # response = self.add_payment(request=request)
-            elif request.data['Function'] == 'ProcessPayment':
-                pass
-                # response = self.process_payment(request=request)
+            if request.data['Function'] == 'ViewType':
+                response = self.view_type(request=request)
+            elif request.data['Function'] == 'AddType':
+                response = self.add_type(request=request)
+            elif request.data['Function'] == 'UpdateType':
+                response = self.update_type(request=request)
+            elif request.data['Function'] == 'RemoveType':
+                response = self.remove_type(request=request)
             else:
                 response = Response({
                     'message': 'Invalid function.'
